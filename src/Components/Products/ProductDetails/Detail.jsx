b3f1de10b69507b7d244452879b7538e1b970fb6
kritika lohiya <you@example.com>
2021-06-07 10:21:16 +0530

    7june

--- src/Components/Products/ProductDetails/Detail.jsx
+++ src/Components/Products/ProductDetails/Detail.jsx
@@ -4,9 +4,9 @@ import ProductInfo from './ProductInfo';
 
 const Detail = ({ id, pname, rate, quantity, imgsrc ,desc}) => {
     return (
-        <div className="mt-5" id={id}>
+        <div className="mb-5 py-5" id={id}>
             <h1 className="my-5 text-center">{pname} Details</h1>
-            <div className="row">
+            <div className="row mb-5">
                 <div className="col col-md-4 d-flex justify-content-center">
                     <ProductImage imgsrc={imgsrc} />
                 </div>
